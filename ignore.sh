0c15b98d814987da7745c0b6d6e8b64c944d056e
Nityansh <nityansh.bhardwaj@gmail.com>
2025-02-21 18:50:28 +0530

    Ignored unnecessary files

---
+++ ignore.sh
@@ -0,0 +1,16 @@
+# Ignore system and config files
+.bash_history
+.bash_logout
+.bashrc
+.cache/
+.config/
+.gitconfig
+.gnupg/
+.lesshst
+.local/
+.profile
+.ssh/
+.sudo_as_admin_successful
+.viminfo
+snap/
+
